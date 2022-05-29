a28b7d60e4fd22c182a0392576d800c367bb0493
Binhao Qin <bhqin@umich.edu>
2022-05-29 13:36:33 -0400

    Adapt to new Interface

--- plugins/init.lua
+++ plugins/init.lua
@@ -14,11 +14,6 @@ return {
       config = function()
          require("neoscroll").setup()
       end,
-
-      -- lazy loading
-      setup = function()
-         nvchad.packer_lazy_load "neoscroll.nvim"
-      end,
    },
    ["luukvbaal/stabilize.nvim"] = {
       config = function()
