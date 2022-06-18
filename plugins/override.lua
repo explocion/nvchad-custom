79f0d8d166a6897deab1609cdba6ae9b7ab75b1f
Binhao Qin <bhqin@umich.edu>
2022-06-18 13:11:16 -0400

    Reformat Structure and Lazy Loading

--- plugins/override.lua
+++ plugins/override.lua
@@ -1,17 +1,33 @@
-return {
-  ["nvim-treesitter/nvim-treesitter"] = {
-         ensure_installed = {
-            "lua",
-            "python",
-            "bash",
-            "c",
-            "cpp",
-            "cuda",
-            "toml",
-            "yaml",
-            "rust",
-            "latex",
-            "markdown",
-         },
-      },
+local M = {}
+
+M["nvim-treesitter/nvim-treesitter"] = {
+   ensure_installed = {
+      "lua",
+      "python",
+      "bash",
+      "c",
+      "cpp",
+      "cuda",
+      "toml",
+      "yaml",
+      "rust",
+      "latex",
+      "markdown",
+   },
 }
+
+-- local present, cmp = pcall(require, "cmp")
+--
+-- if present then
+--    M["hrsh7th/nvim-cmp"] = {
+--       mapping = {
+--          ["<C-CR>"] = cmp.mapping.confirm {
+--             behavior = cmp.ConfirmBehavior.Insert,
+--             select = true,
+--          },
+--          ["<CR>"] = cmp.mapping.close(),
+--       },
+--    }
+-- end
+
+return M
